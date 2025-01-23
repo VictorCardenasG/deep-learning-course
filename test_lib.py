606c4699072a41f15849749adbb5f713bec58281
VictorCardenasG <ar702563@iteso.mx>
2025-01-23 11:21:36 -0600

    Added script to verify library installations.

---
+++ test_lib.py
@@ -0,0 +1,14 @@
+import numpy
+import pandas
+import matplotlib
+import seaborn
+import scipy
+import sklearn
+
+print("Versiones instaladas:")
+print(f"numpy: {numpy.__version__}")
+print(f"pandas: {pandas.__version__}")
+print(f"matplotlib: {matplotlib.__version__}")
+print(f"seaborn: {seaborn.__version__}")
+print(f"scipy: {scipy.__version__}")
+print(f"scikit-learn: {sklearn.__version__}")
